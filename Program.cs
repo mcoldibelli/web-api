47638684a99a664d98caf2ef719624763b4247a4
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-06-24 19:32:55 -0300

    chore: add dependencies

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
